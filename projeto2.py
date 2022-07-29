c81c1222faed772b2f74718914c121608bfcb5d3
Elizangela Camargo <40433498+elizangela-camargo@users.noreply.github.com>
2022-07-28 21:52:57 -0300

    deixando a tela mais limpa.
    apenas deixando a a tela mais limpa!

--- projeto2.py
+++ projeto2.py
@@ -1,40 +1,127 @@
-import os
-from datetime import datetime
 import csv
+from datetime import datetime
+import os
 
-class Questionário:
+class Tabela():
 
-    """ def __init__(self, idade, genero):
-        self.idade = idade
-        self.genero = genero """
+  def __init__(self):
+    #Escolha uma das opções abaixo (nome fixo ou escolher o nome do arquivo), e comente a outra.
+    print('Questionário para levantamento de dados, sobre a vacinação de COVID-19')
+    self.file = 'pesquisa_covid.csv'
+    #self.file = (input('Digite o nome do arquivo (sem extensão) em que deseja escrever as respostas.') + '.csv')
 
-    def perguntas(self, idade, genero, pergunta1, pergunta2, pergunta3, pergunta4,datahora):
-                self.idade = idade
-                self.genero = genero
-                self.pergunta1 = pergunta1
-                self.pergunta2 = pergunta2
-                self.pergunta3 = pergunta3
-                self.pergunta4 = pergunta4
-                self.datahora = datahora
-
-lista_entrevistados = []
-for i in range(3):
-    idade = input("informe sua idade: ")
-    genero = input("informe seu genero: ")
-    pergunta1 = input('Você já contraiu covid?')
-    pergunta2 = input('Caso tenha contraído. Teve alguma sequela?')
-    pergunta3 = input('Você tomou a vacina?')
-    pergunta4 = input('Se tiver tomado, completou o esquema vacinal?')
-    datahora = datetime.today()
-    os.system("cls")
-    temp = Questionário()
-    temp.perguntas(idade, genero, pergunta1, pergunta2, pergunta3, pergunta4,datahora)
-    lista_entrevistados.append(temp)
+    self.lista_respostas = []
     
-print(lista_entrevistados)
 
 
-with open ('respostas_covid.csv', 'w', newline = '') as arq:
-    my_writer = csv.writer(arq, delimiter = ' ')
-    my_writer.writerow(lista_entrevistados)
+  def escrevendo_linhas(self):
+    """Abrindo o arquivo CSV e escrevendo através de um loop"""
 
+    with open(self.file, 'w', newline='') as csvfile:
+
+        while len(self.lista_respostas) < 10:
+          
+          dd = Dados()
+          dd.setAll()
+          
+
+          self.lista_respostas.append(dd.todas_respostas_do_participante())
+          
+
+          escrevendo = csv.writer(csvfile, delimiter=',')
+          escrevendo.writerow(dd.todas_respostas_do_participante())
+
+    csvfile.close()
+
+
+
+class Dados():
+
+  
+  PERGUNTAS = ['Você já contraiu Covid-19?: ', 'Caso tenha contraído Covid-19, apresentou alguma sequela? ',
+              'Você tomou a vacina? ', 'Se tiver se vacinado, completou o esquema vacinal? ']
+  OPCOES = "\n1 - SIM\n2 - NÃO\n3 - NÃO SEI\n"
+  RESPOSTASPOSSIVEIS = ["1", "2", "3"]
+  GENEROS = ['M', 'F', 'NB']
+  
+
+  def __init__(self):
+    self.idade = int(input('Digite a Idade do participante: '))
+
+
+
+  @property
+  def idade(self):
+    return self._idade
+
+  @idade.setter
+  def idade(self, value):
+      '''Verifica e atribui valor à idade.'''
+      while (value < 14) or (value > 130):
+          if value == 00:
+            print(
+                'Idade 00 digitada. Agradecemos pela participação. Pesquisa finalizada.')
+            exit()
+
+          print('Idade inválida para a pesquisa. Verifique o número digitado e a idade do participante. ')
+          value = int(input('Insira a idade do entrevistado: '))
+
+      self._idade = value
+      os.system("cls")
+
+  @property
+  def genero(self):
+    return self._genero
+  
+  @genero.setter
+  def genero(self, gen):
+    '''Verifica e seta o gênero do participante'''
+    while gen not in self.GENEROS:
+      gen = input('\nOpção inválida.\nDigite uma das opções (M - F - NB): \n>> ').upper()
+
+    self._genero = gen
+    os.system("cls")
+
+
+  @property
+  def respostas(self):
+    return self._respostas
+
+  @respostas.setter
+  def respostas(self, ignorar): #Ignorar existe aqui para poder realizar a atribuição de respostas no método setAll e ele identificar a operação como um setter.
+    '''Faz as perguntas ao usuário, verifica a entrada para cada pergunta, e retorna uma lista com suas respostas.'''
+
+    respostas_individuo = []
+
+    for i in range(len(self.PERGUNTAS)):
+      resposta_temporaria = input(self.PERGUNTAS[i] + self.OPCOES)
+
+      while resposta_temporaria not in self.RESPOSTASPOSSIVEIS:
+        print("\nResposta inválida\n")
+        resposta_temporaria = input(self.PERGUNTAS[i] + self.OPCOES)
+
+      respostas_individuo.append(resposta_temporaria)
+      os.system("cls")
+    self._respostas = ','.join(respostas_individuo)
+
+
+
+  def setAll(self):
+    '''Seta todos os elementos de UMA pessoa (Instância de Dados)'''
+
+    self.genero = input('Digite o gênero do entrevistado (M - F - NB): ').upper()
+
+    self.respostas = None
+
+    self._datahora = datetime.today().strftime('%d-%m-%y %H:%M')
+
+
+
+  def todas_respostas_do_participante(self):
+    '''retorna todos os elementos de um participante da pesquisa'''
+
+    return self._idade, self._genero, self._respostas, self._datahora
+
+
+
+Tabela().escrevendo_linhas()
